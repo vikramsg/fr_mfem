80ddb48c87f9ba204224650fc7c17a23c91c786b
VSingh <vikramsingh001@gmail.com>
2017-12-28 15:34:03 +0200

    Adding Periodic analysis functions for Channel flow

--- examples/CNS/cns.hpp
+++ examples/CNS/cns.hpp
@@ -6,11 +6,14 @@
 using namespace std;
 using namespace mfem;
 
+// Function definitions
+
+// Restart Functions
 void writeRestart(Mesh &mesh, GridFunction &u_sol, int cycle, double time);
 void doRestart(const int cycle, ParMesh &pmesh, ParGridFunction &u_sol,
         double &time, int &time_step);
 
-
+// Visualization functions
 void postProcess(Mesh &mesh, int order, const double gamm, const double R_gas, 
         GridFunction &u_sol, GridFunction &aux_grad,
         int cycle, double time);
@@ -19,8 +22,15 @@ void getMoreFields(const double gamm, const double R_gas,
                 Vector &p, Vector &T, Vector &E, Vector &u, Vector &v, Vector &w,
                 Vector &vort, Vector &q);
 
+// Analysis functions
+void ComputeGlobPeriodicMean(MPI_Comm &comm, const vector< vector<int> > &ids, const vector<double> &loc_u_mean, 
+        vector<double> &glob_u_mean);
+void ComputePeriodicMean(int dim, const GridFunction &uD, const vector< vector<int> > &ids, vector<double> &u_mean);
+void GetPeriodicIds(const FiniteElementSpace &fes, const vector<double> &y_unique, vector< vector<int> > &ids);
+void GetUniqueY(const FiniteElementSpace &fes, vector<double>  &y_uni);
 
 
+// Functions
 void doRestart(const int cycle, ParMesh &pmesh, ParGridFunction &u_sol,
         double &time, int &time_step)
 {
@@ -65,7 +75,7 @@ void postProcess(Mesh &mesh, int order, const double gamm, const double R_gas,
    int dim     = mesh.Dimension();
    int var_dim = dim + 2;
 
-   DG_FECollection fec(order , dim);
+   DG_FECollection fec(order + 1 , dim);
    FiniteElementSpace fes_post(&mesh, &fec, var_dim);
    FiniteElementSpace fes_post_grad(&mesh, &fec, (dim+1)*dim);
 
@@ -204,5 +214,162 @@ void getMoreFields(const double gamm, const double R_gas,
         q[i]      = 0.5*(omega_sq - s_sq);
 
     }
+}
+
+
+void ComputeGlobPeriodicMean(MPI_Comm &comm, const vector< vector<int> > &ids, const vector<double> &loc_u_mean, 
+        vector<double> &glob_u_mean)
+{
+    int rank, size;
+    MPI_Comm_rank(comm, &rank);
+    MPI_Comm_size(comm, &size);
+
+    vector<double> glob_u;
+
+    int vert_nodes = ids.size();
+    for(int i = 0; i < vert_nodes; i++)
+    {
+        int loc_row_size = ids.at(i).size();            
+        double loc_row_u = loc_u_mean.at(i)*loc_row_size;
+
+        int glob_row_size ;            
+        double glob_row_u ;
+
+        MPI_Allreduce(&loc_row_u,    &glob_row_u,    1, MPI_DOUBLE, MPI_SUM, comm); // Get global u_max across processors
+        MPI_Allreduce(&loc_row_size, &glob_row_size, 1, MPI_INT,    MPI_SUM, comm); // Get global u_max across processors
+
+        glob_u.push_back(glob_row_u/double(glob_row_size));
+
+        cout << i << "\t" << glob_u.at(i) << endl; 
+
+    }
+    glob_u_mean = glob_u;
+}
+
+
+void ComputePeriodicMean(int dim, const GridFunction &uD, const vector< vector<int> > &ids, vector<double> &u_mean)
+{
+   vector<double> u_m;
+
+   int var_dim = dim + 2;
+   int offset  = uD.Size()/var_dim;
+
+   int vert_nodes = ids.size();
+   for(int i = 0; i < vert_nodes; i++)
+   {
+       vector<int> row_ids = ids.at(i);          
+       int row_nodes = row_ids.size();
+       double u = 0.0;
+       for(int j = 0; j < row_nodes; j++)
+       {
+           int sub = row_ids.at(j);       
+           u      += uD[offset + sub];
+       }
+
+       if (row_nodes > 0)
+           u = u/double(row_nodes);
+       else
+           u = 0;
+
+       u_m.push_back(u);
+   }
+
+   u_mean = u_m;
+
+}
+
+
+
+void GetPeriodicIds(const FiniteElementSpace &fes, const vector<double> &y_unique, vector< vector<int> > &ids)
+{
+   double eps = 1E-11;
+
+   Mesh *mesh = fes.GetMesh();
+   int   dim  = mesh->Dimension();
+   int meshNE = mesh->GetNE();
+
+   FiniteElementSpace fes_nodes(mesh, fes.FEColl(), dim);
+   GridFunction nodes(&fes_nodes);
+   mesh->GetNodes(nodes);
+
+   int nodeSize        = nodes.Size()/dim ;
+
+   std::vector<double> x, y;
+
+   for(int i = 0; i < nodeSize; i++)
+   {
+       x.push_back(nodes(i));
+       y.push_back(nodes(nodeSize + i));
+   }
+
+   vector< vector<int> > l_ids;
+
+   // Now we create a 2D vector which has the node numbers of each unique y
+   for(int j = 0; j < y_unique.size(); j++)
+   {
+       vector<int>  row_ids;
+       for(int i = 0; i < nodeSize; i++)
+       {
+           if (abs(y.at(i) - y_unique.at(j)) < eps)
+               row_ids.push_back(i);
+       }
+       l_ids.push_back(row_ids);
+   }
+   ids = l_ids;
+}
+
+
+
+/*
+ * We'll assume a structured grid in terms of shape for now to make it easier
+ * This is for the Turbulent Channel flow where we average over
+ * the streamwise and spanwise directions
+ * The algorithm is sensitive to Tolerance levels
+ * We simply get all unique y values and then get their subscripts 
+ */
+void GetUniqueY(const FiniteElementSpace &fes, vector<double> &y_uni)
+{
+   double eps = 1E-11;
+
+   Mesh *mesh = fes.GetMesh();
+   int   dim  = mesh->Dimension();
+   int meshNE = mesh->GetNE();
+
+   FiniteElementSpace fes_nodes(mesh, fes.FEColl(), dim);
+   GridFunction nodes(&fes_nodes);
+   mesh->GetNodes(nodes);
+
+   int nodeSize        = nodes.Size()/dim ;
+
+   std::vector<double> x, y, y_orig;
+
+   for(int i = 0; i < nodeSize; i++)
+   {
+       x.push_back(nodes(i));
+       y.push_back(nodes(nodeSize + i));
+       y_orig.push_back(nodes(nodeSize + i));
+   }
+
+   double xMin = *(std::min_element(x.begin(), x.end()));
+   std::sort(y.begin(), y.end(), std::less<double>());
+   
+   std::vector<double> y_unique;
+   y_unique.push_back(y[0]);
+
+   int yCoun = 0; //Count the number of co-ordinates in y direction on which we'll average
+   for(int i = 0; i < nodeSize; i++)
+   {
+       double last_y = y_unique.back(); // Last unique y added
+       if (abs(y[i] - last_y) > eps)
+           y_unique.push_back(y[i]);
+
+       if (abs(x[i] - xMin) < eps)
+           yCoun++;
+   }
+
+
+   MFEM_ASSERT(y_unique.size() == yCoun, "Tolerance levels not enough for unique y") ;
+
+   y_uni = y_unique;
 }
 
